faa5d0d0e98cd2d2dbcf760d6dfde897afe1beaf
unknown <abramlujan@gmail.com>
2022-05-01 02:46:19 -0300

    Handle slashes and add cobra for arg handling

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/nico-abram/goos3
+
+go 1.16
+
+require (
+	github.com/spf13/cobra v1.1.3
+)
--- goos3.go
+++ goos3.go
@@ -1,5 +1,3 @@
-//go:build ignore
-
 package main
 
 import (
@@ -8,76 +6,72 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
-	"net/http/httputil"
+//	"net/http/httputil"
 	"os"
 	"strings"
+	"github.com/spf13/cobra"
 )
+
+func main() {
+	s3Command := &cobra.Command{
+		Use:   "hmns3 [storage folder]",
+		Short: "Run a local s3 server that stores in the filesystem",
+		Run: func(cmd *cobra.Command, args []string) {
+			targetFolder := "./tmp"
+			if len(args) > 0 {
+				targetFolder = args[0]
+			}
+			err := os.MkdirAll(targetFolder, fs.ModePerm)
+			if err != nil {
+				panic(err)
+			}
+
+			handler := func(w http.ResponseWriter, r *http.Request) {
+				bucket, key := bucket_key(r)
+			
+				fmt.Println("\n\nIncoming request path:", r.URL.Path)
+				bodyBytes, err := io.ReadAll(r.Body)
+				fmt.Println("Bucket: ", bucket, " key: ", key, " method: ", r.Method, " len(body): ", len(bodyBytes))
+				if err != nil {
+					panic(err)
+				}
+				if r.Method == http.MethodPut {
+					w.Header().Set("Location", fmt.Sprintf("/%s", bucket))
+					err := os.MkdirAll(fmt.Sprintf("%s/%s", targetFolder, bucket), fs.ModePerm)
+					if err != nil {
+						panic(err)
+					}
+					if key != "" {
+						err = os.WriteFile(fmt.Sprintf("%s/%s/%s",targetFolder, bucket, key),   bodyBytes, fs.ModePerm)
+						if err != nil {
+							panic(err)
+						}
+					}
+				} else if r.Method == http.MethodGet {
+					fileBytes, err := os.ReadFile(fmt.Sprintf("%s/%s/%s",  targetFolder, bucket, key))
+					if err != nil {
+						panic(err)
+					}
+					w.Write(fileBytes)
+				} else {
+					panic("Unimplemented method!")
+				}
+			}
+
+			http.HandleFunc("/", handler)
+			log.Fatal(http.ListenAndServe(":80", nil))
+		},
+	}
+
+	s3Command.Execute()
+}
+
 
 func bucket_key(r *http.Request) (string, string) {
 	slashIdx := strings.IndexByte(r.URL.Path[1:], '/')
 	if slashIdx == -1 {
 		return r.URL.Path[1:], ""
 	} else {
-		return r.URL.Path[1 : 1+slashIdx], r.URL.Path[1+slashIdx:]
+		return r.URL.Path[1 : 1+slashIdx], strings.Replace(r.URL.Path[2+slashIdx:], "/", "~", -1)
 	}
-
-}
-func handler(w http.ResponseWriter, r *http.Request) {
-	reqDump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bucket, key := bucket_key(r)
-
-	fmt.Printf("REQUEST:\n%s", string(reqDump))
-	fmt.Printf("Bucket: %s req %s b %t", bucket, r.Method, r.Method == http.MethodPut)
-	bodyBytes, err := io.ReadAll(r.Body)
-	fmt.Printf("Body: %s", string(bodyBytes))
-	if err != nil {
-		panic(err)
-	}
-	if r.Method == http.MethodPut {
-		w.Header().Set("Location", fmt.Sprintf("/%s", bucket))
-		err := os.MkdirAll(fmt.Sprintf("./tmp/%s", bucket), fs.ModePerm)
-		if err != nil {
-			panic(err)
-		}
-		if key != "" {
-			err = os.WriteFile(fmt.Sprintf("./tmp/%s/%s", bucket, key), bodyBytes, fs.ModePerm)
-			if err != nil {
-				panic(err)
-			}
-		}
-		/*
-		   r.ParseMultipartForm(32 << 20)
-		   var buf bytes.Buffer
-		   file, header, err := r.FormFile("file")
-		   if err != nil {
-		       panic(err)
-		   }
-		   defer file.Close()
-		   name := strings.Split(header.Filename, ".")
-		   fmt.Printf("File name %s\n", name[0])
-		   io.Copy(&buf, file)
-		   contents := buf.String()
-		   fmt.Println(contents)
-		   buf.Reset()
-		*/
-	} else if r.Method == http.MethodGet {
-		fileBytes, err := os.ReadFile(fmt.Sprintf("./tmp/%s/%s", bucket, key))
-		if err != nil {
-			panic(err)
-		}
-		w.Write(fileBytes)
-	}
-}
-
-func main() {
-	err := os.Mkdir("./tmp", fs.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":80", nil))
 }
